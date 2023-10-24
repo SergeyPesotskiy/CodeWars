3d6e0d1fa2ea9e40499e0fe025b60a30b147738b
SergeyPesotskiy <mainerall@gmail.com>
2023-10-24 09:57:40 +0600

    Function 1 - hello world

---
+++ 8 kyu/Function 1 - hello world.js
@@ -0,0 +1,30 @@
+//8 kyu
+//Function 1 - hello world
+//
+//Instructions
+//
+//Make a simple function called greet that returns the most-famous "hello world!".
+//Style Points
+//
+//Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+//
+//Solution
+
+// Write a function "greet" that returns "hello world!"
+
+function greet() {
+  return "hello world!";
+}
+
+//Sample Tests
+//
+//const assert = require("chai").assert;
+//
+//describe("Testing function", function() {
+//  it("Is it a function?", function() {
+//    assert.strictEqual(typeof greet, "function", "greet should be a function");
+//  });
+//  it("Correct return-value?", function() {
+//    assert.strictEqual(greet(), "hello world!");
+//  });
+//});
