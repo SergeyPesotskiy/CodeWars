2fd087e3d2295d18ee2c2e8988545edbf6ef9093
SergeyPesotskiy <mainerall@gmail.com>
2023-10-26 14:13:39 +0600

    MakeUpperCase

---
+++ 8 kyu/MakeUpperCase.js
@@ -0,0 +1,26 @@
+//8 kyu
+//MakeUpperCase
+//
+//Instructions
+//
+//Write a function which converts the input string to uppercase.
+//
+//Solution
+
+function makeUpperCase(str) {
+    // Code here
+    return str.toUpperCase();
+}
+
+//Sample Tests
+//
+//const { assert } = require('chai');
+//
+//describe("Basic Tests", function(){
+//  it("should pass the basic tests", function(){
+//    assert.strictEqual(makeUpperCase(""),           "");
+//    assert.strictEqual(makeUpperCase("hello"), "HELLO");
+//    assert.strictEqual(makeUpperCase("Hello"), "HELLO");
+//    assert.strictEqual(makeUpperCase("HELLO"), "HELLO");
+//  });
+//});
