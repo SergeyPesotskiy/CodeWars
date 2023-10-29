88478c7874cd303b3fdcc5580f197e4ece05d3ad
SergeyPesotskiy <mainerall@gmail.com>
2023-10-29 20:11:01 +0600

    Function 2 - squaring an argument

---
+++ 8 kyu/Function 2 - squaring an argument.js
@@ -0,0 +1,23 @@
+/*8 kyu
+Function 2 - squaring an argument
+
+Instructions
+
+Now you have to write a function that takes an argument and returns the square of it.
+
+Solution*/
+
+// Write the "square"-function here
+function square(n) {
+  return n * n;
+}
+
+/*Sample Tests
+
+const { assert } = require('chai');
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(square(3), 9);
+  });
+});*/
