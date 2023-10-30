63830813418403a9267c2821c89a1452e3b6d2b9
SergeyPesotskiy <mainerall@gmail.com>
2023-10-30 21:00:30 +0600

    Beginner - Reduce but Grow

---
+++ 8 kyu/Beginner - Reduce but Grow.js
@@ -0,0 +1,28 @@
+/*8 kyu
+Beginner - Reduce but Grow
+
+Instructions
+
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+Solution*/
+
+function grow(x){
+  return x.reduce((product, num) => product * num);
+}
+
+/*Sample Tests
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(grow([1, 2, 3]), 6);
+    assert.strictEqual(grow([4, 1, 1, 1, 4]), 16);
+    assert.strictEqual(grow([2, 2, 2, 2, 2, 2]), 64);
+  });
+})*/
